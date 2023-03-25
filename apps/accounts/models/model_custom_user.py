a86f07eb45a7046dd5bdcd90db445df00177e832
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-03-25 19:56:46 +0330

    write model user

--- apps/accounts/models/model_custom_user.py
+++ apps/accounts/models/model_custom_user.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 from apps.accounts.manager import CustomUserManager
 
+
 class CustomUser(AbstractUser):
     """ Custom User class  """
 
@@ -13,6 +14,7 @@ class CustomUser(AbstractUser):
 
     username = None
     email = models.EmailField(_('email'), unique=True)
+    avatar = models.ImageField(_('avatar'), upload_to='accounts/users/avatar', blank=True, null=True)
 
     objects = CustomUserManager()
     USERNAME_FIELD = "email"
