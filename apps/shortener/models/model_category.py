bebbc7e3d9a29559929b2bd112791d23bb6e01b7
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-03-26 22:59:02 +0330

    add __str__ and __save__ in category model

--- apps/shortener/models/model_category.py
+++ apps/shortener/models/model_category.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
 from colorfield.fields import ColorField
-
 
 from apps.utils.models import DateBasic, StatusBasic
 
@@ -14,6 +14,14 @@ class Category(DateBasic, StatusBasic):
 
     title = models.CharField(_('title'), max_length=220)
     slug = models.SlugField(_('slug'), unique=True)
-    color = ColorField(_('Color'),default='#32a852')
+    color = ColorField(_('Color'), default='#32a852')
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_('user'),
                              related_name='categories')
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
