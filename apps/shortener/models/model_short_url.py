ac1f5b1620f9163a38d6d8c7ed30cbad00cd6a2f
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-03-26 22:06:27 +0330

    add custom validator in model

--- apps/shortener/models/model_short_url.py
+++ apps/shortener/models/model_short_url.py
@@ -7,6 +7,8 @@ from apps.utils.models import DateBasic, StatusBasic
 from apps.shortener.utils import generate_short_url
 from apps.shortener.models.model_category import Category
 
+from apps.shortener.validator import NoAspacesAllowed
+
 
 class Shortener(DateBasic, StatusBasic):
     class Meta:
@@ -15,25 +17,47 @@ class Shortener(DateBasic, StatusBasic):
 
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_('user'),
                              related_name='shortener')
-    category = models.OneToOneField(Category, verbose_name='Category',on_delete=models.CASCADE, related_name='shortener', blank=True, null=True)
+    category = models.OneToOneField(Category, verbose_name='Category', on_delete=models.CASCADE,
+                                    related_name='shortener', blank=True, null=True)
     long_url = models.URLField(_('Long URL'))
-    short = models.CharField(_('Short'), max_length=50, blank=True, null=True, unique=True)
+    short = models.CharField(_('Short'), validators=[NoAspacesAllowed], max_length=50, blank=True, null=True,
+                             unique=True)
     password = models.CharField(_('Password'), max_length=15, blank=True, null=True)  # TODO:add validator
     expired_at = models.DateTimeField(_('Expired Time'), blank=True, null=True)
     QR_code = models.ImageField(_('QR Code'), blank=True, null=True, upload_to='shortener/short_urls/QR')
 
-    # TODO:add category fields
 
     def __str__(self):
         return self.short
 
     def save(self, *args, **kwargs):
-        if self.short in None:
+        if not self.short:
             self.short = generate_short_url(length=10)
+
         super().save(*args, **kwargs)
 
+    @property
     def get_expired_at(self):
         return self.expired_at.strftime('%H:%M - %Y/%m/%d')
 
+    @property
     def get_short_url(self):
         return f"https://{Site.objects.get_current().domain}/{self.short}"
+
+    @property
+    def password_active(self):
+        if self.password:
+            return True
+        return False
+
+    @property
+    def QR_code_active(self):
+        if self.QR_code:
+            return True
+        return False
+
+    @property
+    def expired_at_active(self):
+        if self.expired_at:
+            return True
+        return False
