3031530b453fd10a07ef15c6af077a968533f453
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-03-28 18:04:27 +0330

    add jwt in test ShotrenerViewSet

--- apps/shortener/tests/viewset/test_viewset_shortener.py
+++ apps/shortener/tests/viewset/test_viewset_shortener.py
@@ -4,6 +4,7 @@ import json
 from rest_framework import test, status
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from apps.shortener.models import Shortener
 from apps.shortener.api.serializer import (
@@ -18,12 +19,8 @@ class ShortenerViewSetTestCase(test.APITestCase):
         self.user = get_user_model().objects.create(
             email='admin@gmail.com'
         )
-        self.user.set_password('admin')
-        self.user.save()
 
-        self.auth_headers = {
-            'HTTP_AUTHORIZATION': 'Basic ' + base64.b64encode('admin@gmail.com:admin'.encode()).decode()
-        }
+        self.refresh_for_user = RefreshToken.for_user(self.user)
 
         self.shortener = Shortener.objects.create(
             user=self.user,
@@ -57,8 +54,10 @@ class ShortenerViewSetTestCase(test.APITestCase):
     # --------------------------------- List --------------------------------------
     def test_list(self):
         path = reverse('shortener:shortener-list')
-        response = self.client.get(path=path,**self.auth_headers)
-
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f"JWT {self.refresh_for_user.access_token}"
+        )
+        response = self.client.get(path=path)
         shortener = Shortener.objects.all()
         serializer = ShortenerListSerializer(shortener, many=True)
 
@@ -70,12 +69,14 @@ class ShortenerViewSetTestCase(test.APITestCase):
         response = self.client.get(path=path)
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
     # --------------------------------- Create --------------------------------------
 
     def test_create(self):
         path = reverse('shortener:shortener-list')
-        response = self.client.post(path=path,data=self.valid_data, **self.auth_headers)
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f"JWT {self.refresh_for_user.access_token}"
+        )
+        response = self.client.post(path=path,data=self.valid_data)
         content = json.loads(response.content)
 
         serializer = ShortenerCreateUpdateSerializer(data=self.valid_data)
@@ -94,7 +95,10 @@ class ShortenerViewSetTestCase(test.APITestCase):
 
     def test_create_no_valid_data(self):
         path = reverse('shortener:shortener-list')
-        response = self.client.post(path=path, data=self.no_valid_data, **self.auth_headers)
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f"JWT {self.refresh_for_user.access_token}"
+        )
+        response = self.client.post(path=path, data=self.no_valid_data)
 
         serializer = ShortenerCreateUpdateSerializer(data=self.no_valid_data)
         serializer.is_valid(raise_exception=False)
@@ -113,7 +117,10 @@ class ShortenerViewSetTestCase(test.APITestCase):
 
     def test_update(self):
         path = reverse('shortener:shortener-detail',kwargs={'pk':1})
-        response = self.client.put(path=path, data=self.update_valid_date, **self.auth_headers)
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f"JWT {self.refresh_for_user.access_token}"
+        )
+        response = self.client.put(path=path, data=self.update_valid_date)
         content = json.loads(response.content)
 
         serializer = ShortenerCreateUpdateSerializer(data=self.update_valid_date)
@@ -132,7 +139,10 @@ class ShortenerViewSetTestCase(test.APITestCase):
 
     def test_update_no_valid_data(self):
         path = reverse('shortener:shortener-detail',kwargs={'pk':1})
-        response = self.client.put(path=path, data=self.no_update_valid_date, **self.auth_headers)
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f"JWT {self.refresh_for_user.access_token}"
+        )
+        response = self.client.put(path=path, data=self.no_update_valid_date)
 
         serializer = ShortenerCreateUpdateSerializer(data=self.no_update_valid_date)
         serializer.is_valid(raise_exception=False)
@@ -151,7 +161,11 @@ class ShortenerViewSetTestCase(test.APITestCase):
 
     def test_retrieve(self):
         path = reverse('shortener:shortener-detail', kwargs={'pk': 1})
-        response = self.client.get(path=path, **self.auth_headers)
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f"JWT {self.refresh_for_user.access_token}"
+        )
+        response = self.client.get(path=path)
+
 
         shortener = Shortener.objects.get(pk=1)
         serializer = ShortenerDetailSerializer(shortener)
@@ -169,7 +183,10 @@ class ShortenerViewSetTestCase(test.APITestCase):
 
     def test_delete(self):
         path = reverse('shortener:shortener-detail', kwargs={'pk': 1})
-        response = self.client.delete(path=path, **self.auth_headers)
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f"JWT {self.refresh_for_user.access_token}"
+        )
+        response = self.client.delete(path=path)
 
         shortener = Shortener.objects.all().count()
 
