7f46ffb12ba5df9beb2d8be780b91857fc0faf73
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-03-28 20:03:15 +0330

    write router category

--- apps/shortener/api/router.py
+++ apps/shortener/api/router.py
@@ -2,5 +2,6 @@ from rest_framework import routers
 from apps.shortener.api import viewset
 
 router = routers.DefaultRouter()
+router.register('shortener/category', viewset.CategoryViewSet, basename='category')
 router.register('shortener', viewset.ShortenerViewSet, basename='shortener')
 urlpatterns = router.urls
