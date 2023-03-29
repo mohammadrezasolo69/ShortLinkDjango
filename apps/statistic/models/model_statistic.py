abd96808eea28ef13603d119b6425d4f799f3bd0
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-03-29 15:21:10 +0330

    write model statistic

---
+++ apps/statistic/models/model_statistic.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from apps.shortener.models import Shortener
+
+
+class Statistic(models.Model):
+    class Meta:
+        verbose_name = _('Statistic')
+        verbose_name_plural = _('Statistics')
+        ordering = ('-time_click',)
+
+    ip = models.CharField(_('IP'), max_length=45, primary_key=True)
+    os = models.CharField(_('os'), max_length=200)
+    time_click = models.DateTimeField(_('time click'), auto_now_add=True)
+    browser = models.CharField(_('Browser'), max_length=200)
+    country = models.CharField(_('Country'), max_length=200)
+    language = models.CharField(_('Language'), max_length=200)
+    shortener = models.ForeignKey(Shortener, on_delete=models.CASCADE, related_name='statistics',
+                                  verbose_name=_('Shortener'))
+
+    def __str__(self):
+        return self.ip
